2ad4365e0390aa7f1b9c22adb90e63bd8c558fc4
sepehrbz83 <sepehr.sony83@gmail.com>
2026-06-18 03:27:04 +0330

    SQLconnection and aspnetcore

--- obj/Debug/net9.0/Hermes.AssemblyInfo.cs
+++ obj/Debug/net9.0/Hermes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5077a2c17c2cc9a207db0fe92178504a5576fa7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655030a1607ef2908906c2765c87b110f72fb699")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hermes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
